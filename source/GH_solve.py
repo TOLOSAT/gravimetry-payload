db4496d750dc6c27996d576f9dbd81c70c4c2eec
benstyle11 <62807604+benstyle11@users.noreply.github.com>
2021-05-20 18:55:35 +0200

    Update GH_solve.py
    removed potGradMatrix and kept the second version
    replaced phi by theta in the second version

--- source/GH_solve.py
+++ source/GH_solve.py
@@ -32,71 +32,7 @@ import GH_geoMath      as gmath
 # =============================================================================
 # FUNCTIONS FOR Sph Harm SOLVE
 # =============================================================================
-def Get_PotGradMatrix (lmax, Pos): #"R = 6378136.3 m):
-    """
-    Returns the matrix of the gravitational potential gradient.
-    Watch out, it gets big fast.
-    Multiplying it with the appropriate column vector of coefficients
-    will return the acceleration at the given coordinates.
-        *There are no geoid coefficients for l=0, l=1*
-        *There are no sine coefficients for m=0*
-    Input:
-        lmax: max order
-        Pos: array of N_points positions in spherical coordinates (r, theta, phi)
-        *R: Reference radius in meters*
-    Output:
-        M_PotGrad: the matrix of the coefficients
-    """
-    # constants
-    R = 6378.1363 # km
-    GM = 398600.4418 # km**3 s**-2
-    # wiki says : gm = 6.673*10**-11*5.975*10**24 = 398711749999999.94
 
-    N_points = len(Pos) # number of points
-
-    Cos_len = int( (lmax+1)*(lmax+2) /2 ) -3 # c20,c21,c22,c30,c31,c32, ...
-    Sin_len = int( (lmax  )*(lmax+1) /2 ) -1 # s21,s22,s31,s32,s33, ...
-#    print("cos sin lengths =",Cos_len, ",",Sin_len)
-    N_coef = Cos_len + Sin_len
-
-    M_PotGrad = np.ones((N_points * 3, N_coef)) # THE Potential Gradient Matrix
-    print(f"Generating BAM of shape = {M_PotGrad.shape}") # BAM =  "Big Ass Matrix"
-
-    for i in range (0, N_points):
-        term.printProgressBar(i+1, N_points)
-
-        r, theta, phi = Pos[i] #spherical coordinates at the first point
-        Plm_z, Plm_dz = gmath.Pol_Legendre(lmax, lmax, sin(theta))
-
-        j = 0
-        k = Cos_len
-
-        for l in range (2, lmax+1):
-            for m in range (0, l+1):
-                # These equations were found in the GFZ document page 23
-                W_r     = -GM/r**2 * (R/r)**l * (l+1) * Plm_z[m, l]
-                W_phi = GM/r * (R/r)**l * m
-                W_theta   = GM/r * (R/r)**l * Plm_dz[m, l]
-
-                Sub_mat = np.zeros ((3,1))
-                Sub_mat = [ cos(m*phi)*W_r,
-                            cos(m*phi)*W_theta,
-                            -sin(m*phi)*W_phi] # multiply by: COS_lm_coef
-
-                M_PotGrad [3*i : 3*(i+1), j] = Sub_mat
-                j += 1
-
-                # for m of non-null, we get a sine coefficient
-                if (m != 0):
-                    Sub_mat = np.zeros ((3,1))
-                    Sub_mat = [ sin(m*phi)*W_r,
-                                sin(m*phi)*W_theta,
-                                cos(m*phi)*W_phi] # multiply by: SIN_lm_coef
-
-                    M_PotGrad [3*i : 3*(i+1), k] = Sub_mat
-                    k += 1
-
-    return M_PotGrad
 
 
 def Get_PotGradMatrix2 (lmax, Pos): #"R = 6378136.3 m):
@@ -133,7 +69,7 @@ def Get_PotGradMatrix2 (lmax, Pos): #"R = 6378136.3 m):
         term.printProgressBar(i+1, N_points)
 
         r, theta, phi = Pos[i] #spherical coordinates at the first point
-        Plm_z, Plm_dz = gmath.Pol_Legendre(lmax, lmax, sin(phi))
+        Plm_z, Plm_dz = gmath.Pol_Legendre(lmax, lmax, sin(theta))
 
         j = 0
         k = Cos_len
